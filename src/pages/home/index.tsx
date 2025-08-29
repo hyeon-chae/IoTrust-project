a431494d205cfcb76cac8d52ecae86658b93eee1
채현 <52442881+chaengguri@users.noreply.github.com>
2025-08-29 23:42:49 +0900

    Feat/loading (#9)
    * add skeleton
    
    * git ignore 추가
    
    * env 파일 ignore 제거

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,8 @@ import Banner from '@/components/Banner';
 import usePresenter from './usePresenter';
 import { FavoriteList } from '@/components/FavoriteList';
 import { DAppList } from '@/components/DAppList';
+import { BannerSkeleton } from '@/components/ui/skeletons/BannerSkeleton';
+import ListSectionSkeleton from '@/components/ui/skeletons/ListSectionSkeleton';
 
 export default function Home() {
 	const {
@@ -19,9 +21,23 @@ export default function Home() {
 	return (
 		<div className="space-y-6 bg-white min-h-screen max-w-[640px] mx-auto">
 			<div className="space-y-8 pb-20">
-				<Banner data={bannerList} />
-				<FavoriteList data={favoriteList} />
-				<DAppList data={dappList} />
+				{isBannerListFetching || isBannerListError ? (
+					<BannerSkeleton />
+				) : (
+					<Banner data={bannerList} />
+				)}
+
+				{isFavoriteListFetching || isFavoriteListError ? (
+					<ListSectionSkeleton variant="card" rows={3} />
+				) : (
+					<FavoriteList data={favoriteList} />
+				)}
+
+				{isDAppListFetching || isDAppListError ? (
+					<ListSectionSkeleton variant="card" rows={4} rightStub={false} />
+				) : (
+					<DAppList data={dappList} />
+				)}
 			</div>
 		</div>
 	);
