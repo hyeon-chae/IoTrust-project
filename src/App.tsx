7410ee202cf074763137766a24ab19f929eb81ea
changuri <chaehyun282@naver.com>
2025-08-29 19:21:05 +0900

    배너 다국어 적용 및 링크 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import './i18n';
+import { LangProvider } from './contexts/LangContext';
 
 function App() {
 	const [queryClient] = useState(() => new QueryClient());
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<AppRoutes />
-			</BrowserRouter>
-			{import.meta.env.local && <ReactQueryDevtools initialIsOpen={false} />}
+			<LangProvider>
+				<BrowserRouter>
+					<AppRoutes />
+				</BrowserRouter>
+				{import.meta.env.local && <ReactQueryDevtools initialIsOpen={false} />}
+			</LangProvider>
 		</QueryClientProvider>
 	);
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { useLang } from '../contexts/LangContext';
 
 type BannerProps = {
 	data: BannerListResponse;
@@ -11,6 +12,8 @@ type BannerProps = {
 
 export default function Banner({ data }: BannerProps) {
 	const IMG_BASE_URL = import.meta.env.VITE_RESOURCE_URL;
+
+	const { lang } = useLang();
 
 	return (
 		<div className="w-full overflow-hidden mb-6">
@@ -26,16 +29,28 @@ export default function Banner({ data }: BannerProps) {
 			>
 				{data.map((banner) => (
 					<SwiperSlide key={banner.id}>
-						<div className="relative w-full h-[160px] sm:h-[180px] md:h-[200px]">
+						<div
+							className="relative w-full h-[160px] sm:h-[180px] md:h-[200px]"
+							onClick={() => {
+								const link = lang === 'ko' ? banner.link.ko : banner.link.en;
+								!banner.buttonText ? window.open(link, '_blank') : null;
+							}}
+						>
 							<img
-								src={`${IMG_BASE_URL}${banner.image.ko}`}
+								src={`${IMG_BASE_URL}/${
+									lang === 'ko' ? banner.image.ko : banner.image.en
+								}`}
 								alt={banner.title}
-								className="object-cover opacity-20"
+								className="w-full h-full object-cover"
 							/>
 							<div className="absolute left-0 top-0 z-10 flex flex-col justify-between h-full p-4 w-[75%]">
 								{banner.description && (
 									<div className="pt-2">
-										<p className="text-md">{banner.description?.ko}</p>
+										<p className="text-md text-white">
+											{lang === 'ko'
+												? banner.description?.ko
+												: banner.description?.en}
+										</p>
 									</div>
 								)}
 								{banner.buttonText && (
@@ -43,8 +58,15 @@ export default function Banner({ data }: BannerProps) {
 										<Button
 											variant="secondary"
 											className="bg-white text-black rounded-full"
+											onClick={() => {
+												const link =
+													lang === 'ko' ? banner.link.ko : banner.link.en;
+												window.open(link, '_blank');
+											}}
 										>
-											구매하기
+											{lang === 'ko'
+												? banner.buttonText.ko
+												: banner.buttonText.en}
 										</Button>
 									</div>
 								)}
